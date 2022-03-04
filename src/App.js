c81b400525dfa2b4def1924657b4bbf0fbd145fc
ahmedhalilovic <ahmeddhalilovic@gmail.com>
2022-03-04 15:03:41 +0100

    2 files modified, 6 added, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
-import './style.css';
-import HeaderTab from '../testing/Box1';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
+import Blogs from './pages/Blogs';
+import Contact from './pages/Contact';
+import NoPage from './pages/NoPage';
 
 export default function App() {
   return (
-    <div>
-      <HeaderTab />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+ReactDOM.render(<App />, document.getElementById('root'));
