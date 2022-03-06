9e7548277b2db89e32d3d3013b4051342a900727
ahmedhalilovic <ahmeddhalilovic@gmail.com>
2022-03-06 15:54:26 +0100

    3 files modified

--- src/pages/DataLogs.js
+++ src/pages/DataLogs.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const DataLogs = () => {
-  return <h1>Data Logs</h1>;
+  return (
+    <div>
+      <h1>Data Logs</h1>
+    </div>
+  );
 };
 
 export default DataLogs;
