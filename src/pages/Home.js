5cbbe5fa62c513c764936d5c0c7d64757fcb1570
ahmedhalilovic <ahmeddhalilovic@gmail.com>
2022-04-09 23:06:18 +0200

    s

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import Button from '../Button';
+import React from "react";
+import Button from "../Button";
+import "./Home.css";
 
 class Home extends React.Component {
   render() {
     return (
       <div>
         <h1>Home</h1>
-        <div>
-          <Button name="Relay 1" />
-          <Button name="Relay 2" />
-          <Button name="LED lights" />
+        <div className="flexbox-container">
+          <div className="flexbox-item flexbox-item-1">
+            <Button name="Relay 1" />
+            <Button name="Relay 2" />
+            <Button name="LED lights" />
+          </div>
+          <div className="flexbox-item flexbox-item-2"></div>
+          <div className="flexbox-item flexbox-item-3"></div>
         </div>
       </div>
     );
